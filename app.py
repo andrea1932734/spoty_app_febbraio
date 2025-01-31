293c7fbd7fc7a0adebb820ffac3810bfe024dd1b
andrea1932734 <andreacarovelli@gmail.com>
2025-01-31 10:51:56 +0000

    fine

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from spotipy.oauth2 import SpotifyOAuth
 #le tue credenziali le trovi nella dashboard di prima
 SPOTIFY_CLIENT_ID = "774e83e62c3d47aaa319fada2ea26fcc"
 SPOTIFY_CLIENT_SECRET = "6599f2ff0d994565a1975ba8fde8aca0"
-SPOTIFY_REDIRECT_URI = "https://5000-andrea19327-spotyappfeb-jqcfli77crb.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
+SPOTIFY_REDIRECT_URI = "https://5000-andrea19327-spotyappfeb-va08iq8firp.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
 
 app = Flask(__name__)
 app.secret_key = 'chiave_per_session' #ci serve per identificare la sessione
